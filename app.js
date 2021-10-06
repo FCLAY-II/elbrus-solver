61fad05f830845a5b2541b066bdab1408b3db208
Kirill <belokopytovks2014@yandex.ru>
2021-10-06 15:51:21 +0300

    init project

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+const PORT = 3000;
+
+//const app = express();
+//hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'));
+
+//app.set('view engine', 'hbs');
+//app.set('views', path.join(process.env.PWD, 'src', 'views'));
+
+//app.use(express.static(path.join(process.env.PWD, 'public')));
+
+//app.use(express.json()); // <- 'application/json'
+//app.use(express.urlencoded({ extended: true })); // <- 'application/x-www-form-urlencoded'
+
+//app.listen(PORT, () => {
+//  console.log('Server has been started on port', PORT)
+//});
+  
