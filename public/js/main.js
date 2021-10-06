57570679e89d84575ca0774f28f64d688ff79788
Kirill <belokopytovks2014@yandex.ru>
2021-10-06 21:25:09 +0300

    navbar register and login form done

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,63 @@
 const navbar = document.querySelector('.container-fluid');
 const registration = document.querySelector('.center');
-const allBlock = document
+const allBlock = document;
+const login = document.querySelector('.centerlog');
+const truelogin = document.querySelector('.truelogin');
 
-navbar.addEventListener('click',(e)=>{
-  e.preventDefault()
-if(e.target.innerText==="Регистрация"){
-  registration.style.display = 'block'
-  navbar.style.display = 'none'
-  allBlock.addEventListener('click',(e)=>{
-  e.stopPropagation()
-  if(e.target.parentElement=== null){
-  registration.style.display = 'none';
-  navbar.style.display = 'block';
+registration.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.innerText === 'Войти') {
+    login.style.display = 'block';
+    navbar.style.display = 'none';
+    registration.style.display = 'none';
+    allBlock.addEventListener('click', (e) => {
+      e.stopPropagation();
+      if (e.target.parentElement === null) {
+        login.style.display = 'none';
+        navbar.style.display = 'block';
+      }
+    });
   }
-})
-}
-if(e.target.innerText === "Авторизация") {
-  registration.style.display = 'block';
-  navbar.style.display = 'none';
-}
-})
+});
+
+login.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.innerText === 'Создайте аккаунт') {
+    registration.style.display = 'block';
+    navbar.style.display = 'none';
+    login.style.display = 'none'
+    allBlock.addEventListener('click', (e) => {
+      e.stopPropagation();
+      if (e.target.parentElement === null) {
+        registration.style.display = 'none';
+        navbar.style.display = 'block';
+      }
+    });
+  }
+});
+
+navbar.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.innerText === 'Регистрация') {
+    registration.style.display = 'block';
+    navbar.style.display = 'none';
+    allBlock.addEventListener('click', (e) => {
+      e.stopPropagation();
+      if (e.target.parentElement === null) {
+        registration.style.display = 'none';
+        navbar.style.display = 'block';
+      }
+    });
+  }
+  if (e.target.innerText === 'Авторизация') {
+    login.style.display = 'block';
+    navbar.style.display = 'none';
+    allBlock.addEventListener('click', (event) => {
+      event.stopPropagation();
+      if (event.target.parentElement === null) {
+        login.style.display = 'none';
+        navbar.style.display = 'block';
+      }
+    });
+  }
+});
