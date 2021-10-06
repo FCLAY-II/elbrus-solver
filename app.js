3a3947ac798dcb25c9f78ea3ccd75485269ce45f
Kirill <belokopytovks2014@yandex.ru>
2021-10-06 16:20:00 +0300

    new branch init

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ const hbs = require('hbs');
 
 const PORT = 3000;
 
-//const app = express();
-//hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'));
+const app = express();
+hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'));
 
-//app.set('view engine', 'hbs');
-//app.set('views', path.join(process.env.PWD, 'src', 'views'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(process.env.PWD, 'src', 'views'));
 
-//app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(express.static(path.join(process.env.PWD, 'public')));
 
-//app.use(express.json()); // <- 'application/json'
-//app.use(express.urlencoded({ extended: true })); // <- 'application/x-www-form-urlencoded'
+app.use(express.json()); // <- 'application/json'
+app.use(express.urlencoded({ extended: true })); // <- 'application/x-www-form-urlencoded'
 
-//app.listen(PORT, () => {
-//  console.log('Server has been started on port', PORT)
-//});
-  
+app.listen(PORT, () => {
+  console.log('Server has been started on port', PORT);
+});
